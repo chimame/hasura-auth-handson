0d89e933252eb1e73f2915ce4fc69a50798ea53d
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2019-06-30 08:34:44 +0900

    Fix GraphQL

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,91 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
-import './App.css'
-import config from './config'
-import Communities from './Communities'
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import './App.css';
+import config from './config';
+import Communities from './Communities';
 
-firebase.initializeApp(config)
-const provider = new firebase.auth.GoogleAuthProvider()
+firebase.initializeApp(config);
+const provider = new firebase.auth.GoogleAuthProvider();
 
 interface authInterface {
-  status: 'loading' | 'in' | 'out'
-  user?: firebase.User
-  token?: string
+  status: 'loading' | 'in' | 'out';
+  user?: firebase.User;
+  token?: string;
 }
 
 export default () => {
-  const [auth, setAuthState] = useState<authInterface>({status: 'loading'})
+  const [auth, setAuthState] = useState<authInterface>({ status: 'loading' });
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(async user => {
       if (user) {
-        const token = await user.getIdToken()
-        const idTokenResult = await user.getIdTokenResult()
-        const hasuraClaim =
-          idTokenResult.claims["https://hasura.io/jwt/claims"]
-
-        if (hasuraClaim) {
-          setAuthState({ status: "in", user, token })
-        } else {
-          const metadataRef = firebase
-            .database()
-            .ref("metadata/" + user.uid + "/refreshTime")
-
-          metadataRef.on("value", async () => {
-            const token = await user.getIdToken(true)
-            setAuthState({ status: "in", user, token })
-          })
-        }
-      } else {
-        setAuthState({status: 'out'})
+        const token = await user.getIdToken();
+        setAuthState({ status: 'in', user, token });
       }
-    })
-  }, [])
+    });
+  }, []);
 
   const handleSignIn = async () => {
     try {
-      await firebase.auth().signInWithPopup(provider)
+      await firebase.auth().signInWithPopup(provider);
     } catch (e) {
-      console.log(e.message)
+      console.log(e.message);
     }
-  }
-  
+  };
+
   const handleSignOut = async () => {
     try {
-      setAuthState({status: 'loading'})
-      await firebase.auth().signOut()
-      setAuthState({status: 'out'})
+      setAuthState({ status: 'loading' });
+      await firebase.auth().signOut();
+      setAuthState({ status: 'out' });
     } catch (e) {
-      console.log(e.message)
+      console.log(e.message);
     }
-  }
-  
-  let content
+  };
+
+  let content;
   switch (auth.status) {
     case 'loading':
-      content = <>loading...</>
-      break
+      content = <>loading...</>;
+      break;
     case 'in':
       content = (
         <div>
           <div>
-            {(auth.user || {displayName: null}).displayName}
+            {(auth.user || { displayName: null }).displayName}
             <button onClick={handleSignOut}>SignOut</button>
           </div>
-          <Communities token={auth.token || ""} />
+          <Communities token={auth.token || ''} />
         </div>
-      )
-      break
+      );
+      break;
     default:
-      content = <div><button onClick={handleSignIn}>SignIn</button></div>
-      break
+      content = (
+        <div>
+          <button onClick={handleSignIn}>SignIn</button>
+        </div>
+      );
+      break;
   }
 
-  return (
-    <div className="App">
-      {content}
-    </div>
-  )
-}
+  return <div className="App">{content}</div>;
+};
--- front/src/Communities.tsx
+++ front/src/Communities.tsx
@@ -1,49 +1,50 @@
-import React, { useState, useCallback } from 'react'
-import { ApolloProvider, Subscription, Mutation } from "react-apollo"
-import ApolloClient from "apollo-client"
-import { WebSocketLink } from 'apollo-link-ws'
-import { HttpLink } from 'apollo-link-http'
-import { split } from 'apollo-link'
-import { getMainDefinition } from 'apollo-utilities'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import gql from 'graphql-tag'
-const wsurl = 'ws://localhost:8080/v1alpha1/graphql'
-const httpurl = 'http://localhost:8080/v1alpha1/graphql'
+import React, { useState } from 'react';
+import { ApolloProvider, Subscription, Mutation } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws';
+import { HttpLink } from 'apollo-link-http';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
+const wsurl = 'ws://localhost:8080/graphql';
+const httpurl = 'http://localhost:8080';
 
 const COMMUNITIE_SUB = gql`
   subscription COMMUNITIE {
-    communities {
+    communityAdded {
       name
       description
     }
   }
-`
+`;
 
 const COMMUNITY_MUTATION = gql`
   mutation COMMUNITY($name: String!, $description: String) {
-    insert_communities(objects: [{name: $name, description: $description}]) {
-      returning {
-        name
-        description
-      }
-    }  
+    createCommunity(input: { name: $name, description: $description }) {
+      name
+      description
+    }
   }
-`
+`;
 
 interface CommunityData {
-  name: string
-  description: string
+  name: string;
+  description: string;
 }
 
 interface CommunitiesSubscriptionData {
-  communities: [CommunityData]
+  communityAdded: [CommunityData];
 }
 
-class CommunitiesSubscription extends Subscription<CommunitiesSubscriptionData, {}> {}
+class CommunitiesSubscription extends Subscription<
+  CommunitiesSubscriptionData,
+  {}
+> {}
 class CommunityMutation extends Mutation<CommunityData, CommunityData> {}
 
-export default ({token}: {token: string}) => {
-  const headers = { Authorization: `Bearer ${token}` }
+export default ({ token }: { token: string }) => {
+  const headers = { Authorization: `Bearer ${token}` };
 
   const wsLink = new WebSocketLink({
     uri: wsurl,
@@ -53,69 +54,86 @@ export default ({token}: {token: string}) => {
         headers
       }
     }
-  })
+  });
   const httpLink = new HttpLink({
     uri: httpurl,
     headers
-  })
-  
+  });
+
   const link = split(
     ({ query }) => {
-      const { kind, operation } = getMainDefinition(query)
-      return kind === 'OperationDefinition' && operation === 'subscription'
+      const { kind, operation } = getMainDefinition(query);
+      return kind === 'OperationDefinition' && operation === 'subscription';
     },
     wsLink,
-    httpLink,
-  )
-  
+    httpLink
+  );
+
   const client = new ApolloClient({
     link,
     cache: new InMemoryCache()
-  })
+  });
 
-  const [input, setInput] = useState<CommunityData>({name: "", description: ""})
-  const handleInputChange = (type: 'name' | 'description') => (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput({...input, [type]: e.target.value})
-  }
+  const [input, setInput] = useState<CommunityData>({
+    name: '',
+    description: ''
+  });
+  const handleInputChange = (type: 'name' | 'description') => (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInput({ ...input, [type]: e.target.value });
+  };
 
   return (
     <ApolloProvider client={client}>
       <CommunityMutation mutation={COMMUNITY_MUTATION} variables={input}>
-      {
-        community => {
+        {community => {
           const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault()
-            community()
-          }
+            e.preventDefault();
+            community();
+          };
           return (
             <form onSubmit={handleSubmit}>
               <div>name</div>
-              <div><input type="text" value={input.name} onChange={handleInputChange("name")} /></div>
+              <div>
+                <input
+                  type="text"
+                  value={input.name}
+                  onChange={handleInputChange('name')}
+                />
+              </div>
               <div>description</div>
-              <div><input type="text" value={input.description} onChange={handleInputChange("description")} /></div>
+              <div>
+                <input
+                  type="text"
+                  value={input.description}
+                  onChange={handleInputChange('description')}
+                />
+              </div>
               <button type="submit">登録</button>
             </form>
-          )
-        }
-      }
+          );
+        }}
       </CommunityMutation>
       <CommunitiesSubscription subscription={COMMUNITIE_SUB}>
-        {({data, loading, error}) => {
-          if (loading) return 'loading...'
-          if (error) return 'error'
+        {({ data, loading, error }) => {
+          if (loading) return 'loading...';
+          if (error) return 'error';
 
           return (
             <div>
-              {data ? data.communities.map((community) => 
-                <div>
-                  <p>{community.name}</p>
-                  <p>{community.description}</p>
-                </div>
-              ) : null}
+              {data
+                ? data.communityAdded.map(community => (
+                    <div>
+                      <p>{community.name}</p>
+                      <p>{community.description}</p>
+                    </div>
+                  ))
+                : null}
             </div>
-          )
+          );
         }}
       </CommunitiesSubscription>
     </ApolloProvider>
-  )
-}
+  );
+};
